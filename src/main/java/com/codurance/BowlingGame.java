3dcdaaf73e17e30c4cac83dea16b5dd3556c6a6e
Tom <thomas.spencer@codurance.com>
2020-08-06 18:37:43 +0100

    Add currentRoll

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -1,12 +1,14 @@
 package com.codurance;
 
 public class BowlingGame {
-
-
+  private int[] rolls = new int[21];
+  private int currentRoll = 0;
   private int score = 0;
 
   public void roll(int pins) {
     score += pins;
+
+    rolls[currentRoll++] = pins;
   }
 
   public int score() {
