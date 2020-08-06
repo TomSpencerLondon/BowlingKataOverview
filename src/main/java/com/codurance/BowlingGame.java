ef4d8a86e4c6de4c4a8b24c653b8aff9d2c3f0a3
Tom <thomas.spencer@codurance.com>
2020-08-06 18:45:16 +0100

    Add frames with 2 rolls

--- src/main/java/com/codurance/BowlingGame.java
+++ src/main/java/com/codurance/BowlingGame.java
@@ -3,15 +3,22 @@ package com.codurance;
 public class BowlingGame {
   private int[] rolls = new int[21];
   private int currentRoll = 0;
-  private int score = 0;
 
   public void roll(int pins) {
-    score += pins;
-
     rolls[currentRoll++] = pins;
   }
 
   public int score() {
+    int score = 0;
+
+    int i = 0;
+    for(int frame = 0; frame < 10; frame++){
+
+      score += rolls[i] + rolls[i + 1];
+
+      i += 2;
+    }
+
     return score;
   }
 }
